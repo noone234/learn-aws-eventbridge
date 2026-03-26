360f016ce362b0ed53793f5902910ee2581108a1
Christopher Wolfe <cwolfe@draynow.com>
2026-03-25 21:31:29 -0400

    fix: placeholder API URL in demo script

--- docs/examples/demo_curl.sh
+++ docs/examples/demo_curl.sh
@@ -7,6 +7,6 @@ fi
 FileName="$1"
 
 # Send an API request to POST /orders with a JSON request body
-API_URL='https://89c3l4f2cd.execute-api.us-east-1.amazonaws.com/prod'
+API_URL='https://TODO.execute-api.us-east-1.amazonaws.com/prod'
 set -x
 curl -i -X POST $API_URL/orders -H 'Content-Type: application/json' -d @$FileName
